32ddbaea1c1059ad740af2443a3d994880b71f53
Johann Ramìrez <johann.devfull@gmail.com>
2021-02-17 22:29:45 -0500

    Plantilla principal

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,21 +8,27 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{asset('admin_assets/css/font-awesome.min.css')}}">
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="{{asset('/assets_main/css/styles.css')}}" rel="stylesheet" />
+
+  
+
     
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="{{asset('admin_assets/css/ionicons.min.css')}}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('admin_assets/css/adminlte.css')}}">
+
+
 
     <!-- Scripts -->
     @routes
+
     <script src="{{ asset('js/app.js') }}" defer></script>
+
 </head>
 
 <body class="sidebar-mini sidebar-collapse">
@@ -31,10 +37,17 @@
 <script src="{{ asset('admin_assets/js/jquery.min.js') }}"></script>
 <script src="{{ asset('admin_assets/js/jquery-ui.min.js') }}"></script>
 
-<script>
-    $.widget.bridge('uibutton', $.ui.button)
-</script>
-<script src="{{ asset('admin_assets/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('admin_assets/js/adminlte.js') }}"></script>
+<!-- Bootstrap core JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Third party plugin JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+
+<!-- Contact form JS-->
+<script src="{{asset('/assets_main/mail/jqBootstrapValidation.js') }}"></script>
+<script src="{{asset('/assets_main/mail/contact_me.js') }}"></script>
+<!-- Core theme JS-->
+<script src="{{asset('/assets_main/js/scripts.js') }}"></script>
+
 
 </html>
